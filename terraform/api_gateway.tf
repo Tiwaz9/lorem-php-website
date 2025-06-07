9e443ea9dc45d7d89b98c5e270d9f39bbc76896b
Tiwaz9 <vvvelikov91@gmail.com>
2025-06-07 15:37:46 +0300

    Updated with API

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -35,7 +35,7 @@ resource "aws_lambda_permission" "apigw" {
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.lambda_inventory]
   rest_api_id = aws_api_gateway_rest_api.inventory_api.id
-  stage_name  = "prod"
+  
 }
 
 output "inventory_api_url" {
